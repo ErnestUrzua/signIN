cd03d59e341e3ce14cecc5da8b5ee82f0ad0d1b5
ernesturzua <ernieurzua@gmail.com>
2017-12-15 01:35:54 -0800

    working table page

---
+++ signin/src/components/admin.vue
@@ -0,0 +1,36 @@
+<template>
+<table class="table">
+  <thead class="blue-grey lighten-4" text-align="center">
+    <tr>
+      <th>#</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Telephone</th>
+      <th>Company</th>
+      <th>Official Visit</th>
+      <th>Escort Required</th>
+      <th>Escort Name</th>
+    </tr>
+  </thead>
+  <tbody text-align="center">
+    <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>mtop@gmail.com</td>
+            <td>3108675309</td>
+            <td>Azure</td>
+            <td>true</td>
+            <td>true</td>
+            <td>Bob</td>
+        </tr>
+       
+       
+  </tbody>
+</table>
+</template>  
+
+<style>
+#app {
+  text-align: center;
+}
+</style>                              
